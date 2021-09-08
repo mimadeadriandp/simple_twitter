99e54b23406055cc0608e0033bc45d840964cf01
Made Adrian <martinus.adrian@bsi.co.id>
2021-09-08 19:31:49 +0700

    update launch configuration (flutter target)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,7 +3,7 @@
 export "FLUTTER_ROOT=C:\Users\mimad\flutter"
 export "FLUTTER_APPLICATION_PATH=D:\Post grad\Glints\bsi_attendance_mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib\app\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
